8fe65cde8ed061191d3f408af6d5dad4d067c70d
Pan0pticon-student <josephhazen22@gmail.com>
2024-03-11 21:56:44 +0100

    filename

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -188,7 +188,7 @@ resource "aws_apigatewayv2_integration" "integration" {
 resource "aws_lambda_function" "lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "${path.module}/../Backend/api.py"
+  filename      = "${path.module}/../Backend/api.zip"
   function_name = "http_request_IP-DB"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
